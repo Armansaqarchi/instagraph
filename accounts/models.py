0c2b3f7a5db72b7c51fd545bbc58a996b95e8ee3
Arman <arman.saghari81@gmail.com>
2023-03-28 16:15:13 +0330

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from posts.models import Accounts
+from uuid import uuid4
+# Create your models here.
+
+
+class Post(models.Model):
+    id = models.UUIDField(default = uuid4, null=False, primary_key=True, editable = False, unique=True)
+    user_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    likes = models.PositiveBigIntegerField(default = 0)
+    comments = models.PositiveBigIntegerField(default = 0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=50)
+
+
+
+
+class Story(models.Model):
+    id = models.UUIDField(default = uuid4, null=False, primary_key=True, editable = False, unique=True)
+    user_id = models.ForeignKey(Accounts, on_delete=models.CASCADE) 
+    content_url = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+class comments(models.Model):
+    id = models.UUIDField(default = uuid4, null=False, primary_key=True, editable = False, unique=True)
+    sender_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(to = Post, on_delete=models.CASCADE, related_name="comment_post")
+    content = models.CharField(max_length=300)
+    likes_count = models.PositiveBigIntegerField(default = 0)
+    created_at = models.DateField(auto_now_add=True)    
+
+
+
+class Likes(models.Model):
+    id = models.UUIDField(default = uuid4, null=False, primary_key=True, editable = False, unique=True)
+    user_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+    like_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'likes'
+        ordering = ['like_at']
---
+++ posts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
+
+
+class Accounts(models.Model):
+    id = models.UUIDField(default = uuid4, null=False, primary_key=True, editable = False, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
+    date_of_birth = models.DateField(auto_now_add=True)
+    bio = models.TextField(max_length=500)
+    followers = models.PositiveBigIntegerField(default = 0)
+    following = models.PositiveBigIntegerField(default = 0)
+    posts = models.PositiveBigIntegerField(default = 0)
+    
+
+    class Meta:
+        ordering = ['date_of_birth']
+        db_table = 'accounts'
+
+
+
+class Follows(models.Model):
+    id = models.UUIDField(default = uuid4, null=False, primary_key=True, editable = False, unique=True)
+    follower_id = models.ForeignKey(Accounts, on_delete=models.CASCADE, related_name='follower_id')
+    following_id = models.ForeignKey(Accounts, on_delete=models.CASCADE, related_name='following_id')
+    start_following_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'follows'
+        ordering = ['start_following_at']
+
+
