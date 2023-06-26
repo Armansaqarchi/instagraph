74a3b52f34bc79b2395de216c2ed4d7bd2b3b636
Arman <arman.saghari81@gmail.com>
2023-06-26 22:52:14 +0330

    caching home page implemented

--- django/posts/serializer/post_serializer.py
+++ django/posts/serializer/post_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import Post
+from ..models import Post
 
 
 
--- django/posts/urls.py
+++ django/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import re_path, path
 from .views.asyncView import get_post_image
 from .views.homeView import HomeView
+from django.conf import settings
+from django.views.decorators.cache import cache_page
 
+TTL = settings.DEFAULT_CACHE_TIMEOUT
 
 urlpatterns = [
     re_path(r"^photo$", get_post_image, name="post_image"),
-    path("home", HomeView.as_view(), name="main_page")
+    path("home", cache_page(TTL)(HomeView.as_view()), name="main_page")
 ]
