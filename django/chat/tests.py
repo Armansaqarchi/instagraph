4ec6d8924da44426c7ed36a8f5b2214b8d5705a8
Arman <arman.saqarchi@gmail.com>
2023-07-28 18:15:32 +0330

    testing basics of django channel

--- django/chat/tests.py
+++ django/chat/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from channels.testing import WebsocketCommunicator
+from .consumers import ChatConsumer
 
-# Create your tests here.
+class testChat(TestCase):
+    async def test_chat_consumer(self):
+        communicator = WebsocketCommunicator(ChatConsumer.as_asgi(), "ws://localhost:8000/ws/")
+        communicator.scope["user"] = "test"
+
+        connected, subprotocol = await communicator.connect()
+        assert connected
+
+        await communicator.send_to(text_data="hello")
+        response = await communicator.receive_from()
+        assert response == "hello"
+        await communicator.disconnect()
+
